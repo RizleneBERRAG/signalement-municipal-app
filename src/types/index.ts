26d4da994fb6bce1f5bbc8c0a75a7e737ceb9842
Berrag <berragrizlene1977@gmail.com>
2026-03-11 14:47:46 +0100

    commit

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+export type Incident = {
+  id?: string;
+  description: string;
+  photoUri: string;
+  location: Coordinates;
+  timestamp: number;
+};
+
+export type ApiResponse<T> = {
+  success: boolean;
+  data?: T;
+  error?: string;
+};
